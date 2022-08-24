c3b4a8beeb86cb2599add8f0e7d2110dbd59f997
Karin Hawksworth <karin.hawksworth@gmail.com>
2022-08-24 16:33:42 +0100

    added main elements and styled them

---
+++ script.js
@@ -0,0 +1,4 @@
+// two dialogue options one good and one bad, if good then make love bar go up by 1 point
+// say something good about lizards and you get 2 points!
+// elements on page= lizard, table, background, lovebar, dialogue options, dialogue box
+
